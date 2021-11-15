ee1cb2cfeaaf88637c07a6e8e533d4996a0d0ba2
jocuffey <joanne.cuffey@makingwaves.com>
2021-11-15 10:35:34 +0100

    add subscription to update recipes after creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { API } from 'aws-amplify'
 import { listRecipes } from './graphql/queries'
+import { createRecipe} from './graphql/mutations'
+import { onCreateRecipe } from './graphql/subscriptions'
 import { useEffect } from 'react'
 
 function App() {
@@ -10,9 +12,29 @@ function App() {
       console.log(data)
     }
     pullData()
+
+    const subscription = API.graphql(
+      { query: onCreateRecipe}).subscribe({
+        next: (recipeData) => {
+          pullData()
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    return () => subscription.unsubscribe()
   }, [])
+
+  const createNewRecipe = async () => {
+    const name = prompt('recipe name?')
+    const newRecipe = await API.graphql({ query: createRecipe, variables: { input: {name}}})
+    console.log(newRecipe)
+  }
+  
   return (
-    <h1>Check console for data</h1>
+   <div className ="App">
+     <button onClick={createNewRecipe}>Create recipe</button>
+   </div>
   );
 }
 
